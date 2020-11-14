498f1dde8f8f3ff6cbe5a09a802d478afe8185b6
G Balogh <lefthandgergo@gmail.com>
2020-11-14 19:17:05 +0100

    adding intro text

--- ui.R
+++ ui.R
@@ -1,16 +1,17 @@
 library(shiny)
 library(ggplot2)
+readme <- readLines("main_readme.txt")
 
 navbarPage("Ukrajna jelenlegi és jövő hétre várható ország-besorolásai az új COVID-19 esetszámok alapján",
            tabPanel("Kezdőlap",
-                    fluidRow(h1("Kezdőlap"), readLines("main_readme.txt"))),
+                    fluidRow(h1("Kezdőlap"), p(readme[1]), p(readme[2]), p(readme[3]))),
            tabPanel("Országok aktuális besorolása",
                     sidebarLayout(position = "left",
                                   sidebarPanel(dateInput("origin_date", label = "Számítás kiindulási napja")),
-                                  mainPanel(dataTableOutput('latest_classification_df')))
+                                  mainPanel(DT::dataTableOutput('latest_classification_df')))
             ),
            tabPanel("Várható jövő heti besorolás", 
                     sidebarLayout(position = "left",
                                   sidebarPanel(numericInput("n_pred_days", label = "A kiszámításhoz figyelembe vett elmúlt napok száma", value = 7)),
-                                  mainPanel(dataTableOutput('next_classification_df'))))
+                                  mainPanel(DT::dataTableOutput('next_classification_df'))))
 )
