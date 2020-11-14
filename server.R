d4f0a038ca67d093c995ad78e1cda0c4c86837f8
G Balogh <lefthandgergo@gmail.com>
2020-11-14 17:41:34 +0100

    early, but usable version of the app

--- server.R
+++ server.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(ggplot2)
 library(magrittr)
+library(DT)
 
 function(input, output) {
     # setting today's date ----------
@@ -17,38 +18,61 @@ function(input, output) {
     coviddf <- coviddf[coviddf$Country %in% mutual_countries, ]
     coviddf$Date_reported %<>% as.Date
     
+    # translating country names to Hungarian ---------
+    # cntrys <- read.csv("data/countries.csv", stringsAsFactors = F)# https://stefangabos.github.io/world_countries/
+    # cntrys <- cntrys$hun %>% set_names(cntrys$eng)
+    # names(populations) <- cntrys[names(populations)]
+    
     # running analysis for lastest day of classification ----------
     # might be a bit different from the ones on moz.gov.ua, probably because they got population count data from somewhere else
-    origin_date <- as.Date("2020-11-13")
-    last_14_days <- seq((origin_date-13), (origin_date), by = "day")
-    
-    sapply(mutual_countries, function(cntry) {
-        tmpdf <- coviddf[coviddf$Country == cntry, ]
-        crnt_pop <- populations[cntry]
-        ((tmpdf[tmpdf$Date_reported %in% last_14_days, "New_cases"] %>% sum())/crnt_pop)*100000
-    }) -> vals
+    # reactive({output$latest_classification_df <- renderDataTable(tmpfunc(input$origin_date))})
+    output$latest_classification_df <- renderDataTable({
+        origin_date <- input$origin_date
+        last_14_days <- seq((origin_date-13), (origin_date), by = "day")
+        sapply(mutual_countries, function(cntry) {
+            tmpdf <- coviddf[coviddf$Country == cntry, ]
+            crnt_pop <- populations[cntry]
+            ((tmpdf[tmpdf$Date_reported %in% last_14_days, "New_cases"] %>% sum())/crnt_pop)*100000
+        }) %>% set_names(mutual_countries) -> vals
+        
+        classification <- ifelse(vals > vals["Ukraine"], "red", "green")
+        outdf <- data.frame(countries = mutual_countries, values = round(vals, digits = 2), classification = classification)
+        rownames(outdf) <- NULL
+        outdf <- outdf[order(outdf$values, decreasing = T), ]
+        dt <- outdf %>%
+            datatable(rownames= FALSE, options = list(pageLength = length(mutual_countries), language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Hungarian.json'))) %>%
+            formatStyle("classification", target = "row", backgroundColor = styleEqual(c("red", "green"), c('#F71405', '#05F766')))
+    })
+   
 
-    output$latest_classification_df <- renderDataTable(data.frame(mutual_countries, vals))
+    
 
     # # calculating probable case numbers based on next recategorization based on the trends in the last 28 days ----------
-    # origin_date <- date_today
-    # next_recategorization_date <- as.Date("2020-11-20")
-    # last_14_days <- seq((origin_date-13), (origin_date), by = "day")
-    # last_14_days_from_next_recat <- seq((next_recategorization_date-13), (next_recategorization_date), by = "day")
-    # days_of_prediction <- seq((origin_date-7), (origin_date), by = "day")
-    # next_days <- seq(origin_date+1, next_recategorization_date, by = "day")
-    # 
-    # pbsapply(mutual_countries, function(cntry) {
-    #     tmpdf <- coviddf[coviddf$Country == cntry, ]
-    #     tmpdf_pred <- tmpdf[tmpdf$Date_reported %in% days_of_prediction, ]
-    #     mdl <- lm(New_cases ~ Date_reported, tmpdf_pred)
-    #     predcnt_til_next_recat <- predict(mdl, newdata = data.frame(Date_reported = next_days)) %>% round()
-    #     
-    #     ggplot(data = tmpdf_pred, aes(x = Date_reported, y = New_cases)) + geom_point() + geom_smooth(method = "lm")
-    #     crnt_pop <- populations[cntry]
-    #     ((sum(tmpdf[tmpdf$Date_reported %in% last_14_days_from_next_recat, "New_cases"], predcnt_til_next_recat)/crnt_pop)*100000) %>% format(digits = 3) %>% as.numeric()
-    # }) -> vals
-    # 
-    # output$next_classification_df <- data.frame(mutual_countries, vals) %>% renderDataTable()
-    
+    output$next_classification_df <- renderDataTable({
+        origin_date <- date_today
+        next_recategorization_date <- as.Date("2020-11-20")
+        last_14_days <- seq((origin_date-13), (origin_date), by = "day")
+        last_14_days_from_next_recat <- seq((next_recategorization_date-13), (next_recategorization_date), by = "day")
+        days_of_prediction <- seq((origin_date-input$n_pred_days), (origin_date), by = "day")
+        next_days <- seq(origin_date+1, next_recategorization_date, by = "day")
+        
+        pbsapply(mutual_countries, function(cntry) {
+            tmpdf <- coviddf[coviddf$Country == cntry, ]
+            tmpdf_pred <- tmpdf[tmpdf$Date_reported %in% days_of_prediction, ]
+            mdl <- lm(New_cases ~ Date_reported, tmpdf_pred)
+            predcnt_til_next_recat <- predict(mdl, newdata = data.frame(Date_reported = next_days)) %>% round()
+            
+            ggplot(data = tmpdf_pred, aes(x = Date_reported, y = New_cases)) + geom_point() + geom_smooth(method = "lm")
+            crnt_pop <- populations[cntry]
+            ((sum(tmpdf[tmpdf$Date_reported %in% last_14_days_from_next_recat, "New_cases"], predcnt_til_next_recat)/crnt_pop)*100000) %>% round(digits = 2) %>% as.numeric()
+        }) -> vals
+        
+        classification <- ifelse(vals > vals["Ukraine"], "red", "green")
+        outdf <- data.frame(countries = mutual_countries, values = vals, classification = classification)
+        rownames(outdf) <- NULL
+        outdf <- outdf[order(outdf$values, decreasing = T), ]
+        dt <- outdf %>%
+            datatable(rownames= FALSE, options = list(pageLength = length(mutual_countries), language = list(url = '//cdn.datatables.net/plug-ins/1.10.11/i18n/Hungarian.json'))) %>%
+            formatStyle("classification", target = "row", backgroundColor = styleEqual(c("red", "green"), c('#F71405', '#05F766')))
+    })
 }
--- ui.R
+++ ui.R
@@ -1,18 +1,14 @@
 library(shiny)
 library(ggplot2)
 
-dataset <- diamonds
-
-fluidPage(
-    
-    titlePanel("Predicting UA categories"),
-    
-    sidebarPanel(
-         dateInput("origin_date", label = "Date of origin")
-    ),
-    
-    tabsetPanel(
-        tabPanel("Latest classification", dataTableOutput('latest_classification_df')),
-        tabPanel("Next classification", dataTableOutput('next_classification_df'))
-    )
+navbarPage("Current and predicted next week-classification of countries by UA Ministry of Health based on COVID-19 case numbers",
+           tabPanel("Latest classification",
+                    sidebarLayout(position = "left",
+                                  sidebarPanel(dateInput("origin_date", label = "Date of origin"),),
+                                  mainPanel(dataTableOutput('latest_classification_df')))
+            ),
+           tabPanel("Next classification", 
+                    sidebarLayout(position = "left",
+                                  sidebarPanel(numericInput("n_pred_days", label = "Number of latest days to use during prediction", value = 7)),
+                                  mainPanel(dataTableOutput('next_classification_df'))))
 )
