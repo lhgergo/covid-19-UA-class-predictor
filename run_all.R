85e84105aac51edd418bce278c400884f5ec3cba
G Balogh <lefthandgergo@gmail.com>
2020-11-21 18:24:11 +0100

    creating script

---
+++ run_all.R
@@ -0,0 +1,16 @@
+setwd("~/Dokumentumok/LABOR/covid-19-UA-class-predictor/")
+
+# loading packages ----------
+library(magrittr)
+library(purrr)
+library(pbapply)
+library(ggplot2)
+library(lubridate)
+
+# creating new daily report ----------
+source("collect_current_data.R")
+
+# updating main page ----------
+source("update_main.R")
+
+# pushing everything back to github ----------
